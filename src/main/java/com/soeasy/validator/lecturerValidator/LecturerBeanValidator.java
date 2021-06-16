0c69f7fedce1cfedf1e1168fa196ef29a4d8679b
BettyCoding <weiting1221@gmail.com>
2021-06-17 06:12:27 +0800

    新增講師表單驗證

---
+++ src/main/java/com/soeasy/validator/lecturerValidator/LecturerBeanValidator.java
@@ -0,0 +1,28 @@
+package com.soeasy.validator.lecturerValidator;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+import com.soeasy.model.LecturerBean;
+import com.soeasy.model.ShopBean;
+
+public class LecturerBeanValidator implements Validator {
+
+	@Override
+	public boolean supports(Class<?> clazz) {
+		return LecturerBean.class.isAssignableFrom(clazz);
+	}
+
+	@Override
+	public void validate(Object target, Errors errors) {
+		LecturerBean lecturer = (LecturerBean) target;
+
+		// 欄位不得空白
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "lecturerName", "", "名字不得空白");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "lecturerTalent", "", "專長不得空白");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "lecturerExp", "", "經歷不得空白");
+
+	}
+
+}
