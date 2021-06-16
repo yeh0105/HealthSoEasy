4b51dc86be5ad584d71c4fe78d4257122b09ebef
BettyCoding <weiting1221@gmail.com>
2021-06-17 01:36:33 +0800

    新增講師service

---
+++ src/main/java/com/soeasy/service/lecturerService/LecturerService.java
@@ -0,0 +1,24 @@
+package com.soeasy.service.lecturerService;
+
+import java.util.List;
+
+import com.soeasy.model.LecturerBean;
+import com.soeasy.model.PostBean;
+
+public interface LecturerService {
+	
+	//新增講師
+	void addLecturer(LecturerBean lecturerBean);
+
+	//查詢一筆
+	LecturerBean findByLecturerId(Integer lecturerId);
+
+	//查詢全部
+	List<LecturerBean> findAllByLecturerId();
+
+	//修改
+	void updateLecturer(LecturerBean lecturerBean);
+
+	//刪除
+	void deleteByLecturerId(Integer lecturerId);
+}
