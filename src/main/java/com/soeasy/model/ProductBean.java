cb431e46e7988d1cbf1c55925e7c4542305aa50c
syaoju <senyaky@gmail.com>
2021-06-17 20:35:56 +0800

    no message

--- src/main/java/com/soeasy/model/ProductBean.java
+++ src/main/java/com/soeasy/model/ProductBean.java
@@ -3,18 +3,13 @@ package com.soeasy.model;
 import java.io.Serializable;
 import java.sql.Blob;
 import java.sql.Date;
-import java.util.HashSet;
-import java.util.Set;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Transient;
