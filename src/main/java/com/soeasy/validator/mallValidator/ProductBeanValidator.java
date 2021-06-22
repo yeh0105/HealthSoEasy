1c98d85a66607ab0499722e8f04ce70d0746cf1b
syaoju <senyaky@gmail.com>
2021-06-22 19:29:49 +0800

    1. 新增商城首頁、商品呈列畫面 2.  商城後台微調

--- src/main/java/com/soeasy/validator/mallValidator/ProductBeanValidator.java
+++ src/main/java/com/soeasy/validator/mallValidator/ProductBeanValidator.java
@@ -5,6 +5,7 @@ import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
 import com.soeasy.model.LecturerBean;
+import com.soeasy.model.PostBean;
 import com.soeasy.model.ProductBean;
 import com.soeasy.model.ShopBean;
 
@@ -12,8 +13,9 @@ public class ProductBeanValidator implements Validator {
 
 	@Override
 	public boolean supports(Class<?> clazz) {
-		return ProductBean.class.isAssignableFrom(clazz);
-	}
+		boolean b = ProductBean.class.isAssignableFrom(clazz);
+
+		return b;	}
 
 	@Override
 	public void validate(Object target, Errors errors) {
@@ -23,7 +25,12 @@ public class ProductBeanValidator implements Validator {
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "productName", "", "產品名字不得空白");
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "productDescription", "", "描述不得空白");
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "productPrice", "", "價格不得空白");
-
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "productAmount", "", "不得空白");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "productCost", "", "不得空白");
+//		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "productImg", "", "不得空白");
+		if (product.getProductMultiImg().isEmpty()) {
+			errors.rejectValue("productMultiImg", "", "必須挑選圖片");
+		}
 	}
 
 }
