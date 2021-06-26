aeea61110b9fd570992b85a6460d73b4235c8956
kelly <kelly82711@gmail.com>
2021-06-26 20:29:22 +0800

    地圖前台查詢特定地區(未完成)

--- src/main/java/com/soeasy/service/sportmapService/impl/SportMapServiceImpl.java
+++ src/main/java/com/soeasy/service/sportmapService/impl/SportMapServiceImpl.java
@@ -70,13 +70,13 @@ public class SportMapServiceImpl implements SportMapService {
 		return map;
 	}
 	
-	// 抓資料庫裡全部有幾個地圖
+	// 抓資料庫裡全部有幾個地圖(搭配查詢所有地圖，帶分頁用)
 	@Override
 	public Long getRecordCounts() {		
 		return sportMapRepository.count();
 	}
 
-	// 總共有幾頁
+	// 總共有幾頁(搭配查詢所有地圖，帶分頁用)
 	@Transactional
 	@Override
 	public Integer getTotalPages() {
@@ -85,7 +85,6 @@ public class SportMapServiceImpl implements SportMapService {
 		return totalPages;		
 
 	}	
-
 	
 	//更新地圖資料
 	@Transactional
@@ -107,7 +106,7 @@ public class SportMapServiceImpl implements SportMapService {
 	return sportMapRepository.findAll();
 	}
 
-	// 抓分頁一頁有哪些地圖by SportCategoryId(SportMapBean裡SportCategoryId欄位名稱為SportCategoryBean)
+	// 抓分頁一頁有哪些地圖，依照SportCategoryId(SportMapBean裡SportCategoryId欄位名稱為SportCategoryBean)
 	@Override
 	public Map<Integer, SportMapBean> getPageSportMapsBySportCategoryId(SportCategoryBean SportCategoryBean, Integer pageNo) {
 		Map<Integer, SportMapBean> map = new LinkedHashMap<>();
@@ -121,7 +120,7 @@ public class SportMapServiceImpl implements SportMapService {
 		return map;
 	}
 	
-	// 抓資料庫一個分類裡有幾個地圖
+	// 抓資料庫一個分類裡有幾個地圖(搭配抓分頁一頁有哪些地圖，依照SportCategoryId用)
 	@Override
 	public Long getRecordCountsBySportCategoryId(SportCategoryBean sportCategoryBean) {
 		System.out.println("第二關收到進來的sportCategoryBean="+sportCategoryBean);
@@ -129,37 +128,59 @@ public class SportMapServiceImpl implements SportMapService {
 	}
 
 
-	// 一個分類總共有幾頁
-		@Transactional
-		@Override
-		public Integer getTotalPagesBySportCategoryId(SportCategoryBean sportCategoryBean) {
-			System.out.println("第一關進來的sportCategoryBean="+sportCategoryBean);
-			totalPages = (int) (Math.ceil(getRecordCountsBySportCategoryId(sportCategoryBean) / (double) recordsPerPage));
-			logger.info("totalPages=" + totalPages);
-			System.out.println("印出來的totalPages="+totalPages);
-			return totalPages;		
+	// 一個分類總共有幾頁(搭配抓分頁一頁有哪些地圖，依照SportCategoryId用)
+	@Transactional
+	@Override
+	public Integer getTotalPagesBySportCategoryId(SportCategoryBean sportCategoryBean) {
+		System.out.println("第一關進來的sportCategoryBean="+sportCategoryBean);
+		totalPages = (int) (Math.ceil(getRecordCountsBySportCategoryId(sportCategoryBean) / (double) recordsPerPage));
+		logger.info("totalPages=" + totalPages);
+		System.out.println("印出來的totalPages="+totalPages);
+		return totalPages;		
 
-		}
-		//給特定分數撈出特定分數的前三筆
-		@Override
-		public List<SportMapBean> findFirst3BySportMapScore(Integer sportMapScore, Sort sort) {
-			return sportMapRepository.findFirst3BySportMapScore(sportMapScore, sort);
-		}
+	}
+	
+	//給特定分數撈出特定分數的前3筆
+	@Override
+	public List<SportMapBean> findFirst3BySportMapScore(Integer sportMapScore, Sort sort) {
+		return sportMapRepository.findFirst3BySportMapScore(sportMapScore, sort);
+	}
 
-		//給特定分數撈出特定分數的前三筆
-		@Override
-		public List<SportMapBean> findTop3BySportMapScore(Integer sportMapScore, Sort sort) {
-			return sportMapRepository.findTop3BySportMapScore(sportMapScore, sort);
-		}
+	//給特定分數撈出特定分數的前3筆
+	@Override
+	public List<SportMapBean> findTop3BySportMapScore(Integer sportMapScore, Sort sort) {
+		return sportMapRepository.findTop3BySportMapScore(sportMapScore, sort);
+	}
 
-		//查詢所有地圖裡的前3筆，依照地圖分數降冪排序
-		@Override
-		public List<SportMapBean> findTop3ByOrderBySportMapScoreDesc() {
-			return sportMapRepository.findTop3ByOrderBySportMapScoreDesc();
+	//查詢所有地圖裡的前3筆，依照地圖分數降冪排序
+	@Override
+	public List<SportMapBean> findTop3ByOrderBySportMapScoreDesc() {
+		return sportMapRepository.findTop3ByOrderBySportMapScoreDesc();
+	}
+
+	//抓分頁一頁有哪些地圖，依照區域(地址關鍵字)
+	@Override
+	public Map<Integer, SportMapBean> getPageSportMapsBySportInfoLike(String SportMapInfo, Integer pageNo) {
+		Map<Integer,SportMapBean> map = new LinkedHashMap<>();
+		Pageable pageable = PageRequest.of(pageNo - 1, recordsPerPage);
+		Page<SportMapBean> beans = sportMapRepository.findBySportMapInfoLike(SportMapInfo, pageable);
+		
+		List<SportMapBean> list = beans.getContent();
+		for(SportMapBean bean : list) {
+			map.put(bean.getSportMapId(), bean);
 		}
+		return map;
+	}
+	
+	// 抓資料庫依照區域(地址關鍵字)裡有幾個地圖(搭配抓分頁一頁有哪些地圖，依照區域(地址關鍵字))
+	
+	
+	// 抓依照區域(地址關鍵字)總共有幾頁(搭配抓分頁一頁有哪些地圖，依照區域(地址關鍵字))
 
 		
- 
+
+	
+		
 		
 
 	
