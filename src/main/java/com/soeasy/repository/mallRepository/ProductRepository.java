50c6ec29dda25595cf35d5a36d191dd38eab4de1
syaoju <senyaky@gmail.com>
2021-06-28 19:17:11 +0800

    1. 購物出CRUD 功能完成( 圖片未處理) 2. 產品頁面(查詢\排序)功能完成

--- src/main/java/com/soeasy/repository/mallRepository/ProductRepository.java
+++ src/main/java/com/soeasy/repository/mallRepository/ProductRepository.java
@@ -2,20 +2,32 @@ package com.soeasy.repository.mallRepository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.soeasy.model.ProductBean;
 
 public interface ProductRepository extends JpaRepository<ProductBean, Integer>,PagingAndSortingRepository<ProductBean,Integer>{
 
+
+	//產品模糊查詢
+	//如果使用concat 省略語法會有SQL指令轉換的問題(? 
+	
+	@Query("SELECT p FROM ProductBean p WHERE p.productName Like %?1%"
+	+ " OR p.productPrice Like %?1%"
+	+ " OR p.productId Like %?1%"
+	+ " OR p.productAmount Like %?1%"
+	+ " OR p.productDescription Like %?1%"
+	+ " OR p.productCalories Like %?1%"
+	+ " OR p.productCategory Like %?1%"
+	+ " OR p.category Like %?1%"  
+	+ " OR p.productDate Like %?1%")
+	public Page<ProductBean> findAll(String keyword,Pageable pageable);
 	
 	List<ProductBean> findByProductName(String productName);
-	List<ProductBean> findByProductDescriptionContains(String productDescription);
-	
-//	//自定義查詢，有問題還要調整
-//	@Query("select b form product b where b")
-//	List<ProductBean> findByJPQL(int len);
-	
+
 	
 }
