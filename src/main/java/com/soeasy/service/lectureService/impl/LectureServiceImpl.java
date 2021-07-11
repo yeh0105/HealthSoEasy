860c8620b1c513a48e91c3827e8a6928f381d28b
BettyCoding <weiting1221@gmail.com>
2021-07-11 19:58:18 +0800

    講師路徑bug解決、講師綠底表頭bug解決、講座詳細資訊頁面完成。

--- src/main/java/com/soeasy/service/lectureService/impl/LectureServiceImpl.java
+++ src/main/java/com/soeasy/service/lectureService/impl/LectureServiceImpl.java
@@ -3,19 +3,32 @@ package com.soeasy.service.lectureService.impl;
 import java.sql.Date;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.soeasy.model.LectureBean;
+import com.soeasy.model.NutritionistBean;
 import com.soeasy.repository.lectureRepository.LectureRepository;
 import com.soeasy.service.lectureService.LectureService;
 import com.soeasy.service.lecturerService.LecturerService;
+import com.soeasy.service.postService.impl.PostServiceImpl;
 import com.soeasy.util.GlobalService;
 
 @Transactional
 @Service
 public class LectureServiceImpl implements LectureService {
+
+	// 建立Logger
+	private static final Logger logger = LoggerFactory.getLogger(LectureServiceImpl.class);
+
+	private Integer recordsPerPage = GlobalService.LECTURE_PER_PAGE;
+	private Integer totalPages = -1;
 
 	@Autowired
 	LectureRepository lectureRepository;
@@ -58,8 +71,8 @@ public class LectureServiceImpl implements LectureService {
 	public List<LectureBean> updateLectureStatus() {
 		List<LectureBean> lectures = lectureRepository.findAll();
 		for (LectureBean lectureBean : lectures) {
-			
-			//以下依日期判斷
+
+			// 以下依日期判斷
 			Date date = new Date(System.currentTimeMillis());
 			if ((date.getTime() - lectureBean.getLectureDate().getTime()) / (86400000 * 1) >= 1) {
 				System.out.println("精彩回顧");
@@ -76,8 +89,30 @@ public class LectureServiceImpl implements LectureService {
 		return lectures;
 	}
 
+	// 依據講座狀態自動進入輪播
 	@Override
 	public List<LectureBean> findByLectureStatus(Integer lectureStatus) {
 		return lectureRepository.findByLectureStatus(lectureStatus);
+	}
+
+	//==================分頁區==================
+	@Override
+	public List<LectureBean> getAllPageLecture(Integer pageNo) {
+		Pageable pageable = PageRequest.of(pageNo - 1, recordsPerPage);
+		Page<LectureBean> beans = lectureRepository.findAll(pageable);
+		List<LectureBean> list = beans.getContent();
+		return list;
+	}
+
+	@Override
+	public Long getRecordCounts() {
+		return lectureRepository.count();
+	}
+
+	@Override
+	public Integer getTotalPages() {
+		totalPages = (int) (Math.ceil(getRecordCounts() / (double) recordsPerPage));
+		logger.info("totalPages=" + totalPages);
+		return totalPages;
 	}
 }
